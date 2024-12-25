86b80a307cd2d049011f58162bf51d7fe2e87986
bprince969 <65220343+bprince969@users.noreply.github.com>
2024-12-25 15:42:47 +0530

    Update mysql.db.js

--- src/config/mysql.db.js
+++ src/config/mysql.db.js
@@ -3,7 +3,7 @@ const { CONFIG } = require("./index")
 const mySqlPromise = require("mysql2/promise");
 
 const pool = 
-mySqlPromisemysql.createPool(
+mySqlPromise.createPool(
     `${CONFIG.DATABASE_URL}?multipleStatements=true&dateStrings=true&waitForConnections=true&connectionLimit=99&enableKeepAlive=true&keepAliveInitialDelay=10000`
 );
 console.log(`DB Pool Created.`);
